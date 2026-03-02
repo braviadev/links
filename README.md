7b8c39c1f791f92665facd247072f0f11ea1cac5
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-02 22:15:14 +0100

    Initial production-ready upload

---
+++ README.md
@@ -0,0 +1,40 @@
+<p align="center">
+  <img src="public/icon.png" height="128">
+  <h1 align="center">Links</h1>
+</p>
+
+Links is a linktree-like application built with Next.js and Tailwindcss. It allows you to create a personalized page with links to your social media profiles, websites, and other online presence.
+
+## Getting Started
+
+Follow these steps to run the project locally on your machine:
+
+1. Clone the repository.
+
+```bash
+git clone https://github.com/nelsonlaidev/links.git
+```
+
+2. Navigate to the project directory.
+
+```bash
+cd links
+```
+
+3. Install dependencies.
+
+```bash
+pnpm install
+```
+
+4. Run the development server.
+
+```bash
+pnpm dev
+```
+
+---
+
+<p align="center">
+Made with ❤️ in Hong Kong
+</p>
---
+++ src/app/page.tsx
@@ -0,0 +1,110 @@
+import Image from 'next/image'
+import { FiBook } from 'react-icons/fi'
+import {
+  SiDevdotto,
+  SiDiscord,
+  SiFacebook,
+  SiGithub,
+  SiInstagram,
+  SiStackoverflow,
+  SiSteam,
+  SiX,
+  SiYoutube
+} from 'react-icons/si'
+
+import PrimaryLinks from '@/components/primary-links'
+import Spotlight from '@/components/spotlight'
+
+const links = [
+  {
+    icon: <FiBook className='text-zinc-300' />,
+    title: 'Blog',
+    url: 'https://nelsonlai.link/s/blog'
+  },
+  {
+    icon: <SiYoutube className='text-zinc-300' />,
+    title: 'YouTube',
+    url: 'https://nelsonlai.link/s/youtube'
+  },
+  {
+    icon: <SiFacebook className='text-zinc-300' />,
+    title: 'Facebook',
+    url: 'https://nelsonlai.link/s/facebook'
+  },
+  {
+    icon: <SiSteam className='text-zinc-300' />,
+    title: 'Steam',
+    url: 'https://nelsonlai.link/s/steam'
+  },
+  {
+    icon: <SiInstagram className='text-zinc-300' />,
+    title: 'Instagram',
+    url: 'https://nelsonlai.link/s/instagram'
+  },
+  {
+    icon: <SiGithub className='text-zinc-300' />,
+    title: 'GitHub',
+    url: 'https://nelsonlai.link/s/github'
+  },
+  {
+    icon: <SiDiscord className='text-zinc-300' />,
+    title: 'Discord',
+    url: 'https://nelsonlai.link/s/discord'
+  },
+  {
+    icon: <SiX className='text-zinc-300' />,
+    title: 'X',
+    url: 'https://nelsonlai.link/s/twitter'
+  },
+  {
+    icon: <SiStackoverflow className='text-zinc-300' />,
+    title: 'Stack overflow',
+    url: 'https://nelsonlai.link/s/stackoverflow'
+  },
+  {
+    icon: <SiDevdotto className='text-zinc-300' />,
+    title: 'Dev.to',
+    url: 'https://nelsonlai.link/s/devto'
+  }
+]
+
+const Page = () => {
+  return (
+    <>
+      <div className='relative mx-auto flex flex-col items-center justify-center gap-4 pt-24 pb-10'>
+        <Spotlight className='-top-4 left-56' />
+        <Image
+          src='https://nelsonlai.dev/images/avatar.png'
+          width={90}
+          height={90}
+          alt='Logo'
+          className='rounded-full'
+          priority
+        />
+        <h1 className='text-xl font-semibold'>Nelson Lai</h1>
+        <p className='text-[#a1a1a1]'>full stack engineer</p>
+      </div>
+      <PrimaryLinks />
+      <div className='flex flex-col gap-4 py-3'>
+        {links.map((link) => {
+          const { icon, title, url } = link
+
+          return (
+            <a
+              key={url}
+              href={url}
+              className='relative flex h-14 w-full items-center justify-center rounded-xl border border-zinc-700 bg-[#151414] px-8 py-4 transition-colors duration-300 hover:border-zinc-500'
+              target='_blank'
+              rel='noopener, noreferrer'
+            >
+              <div className='absolute left-8'>{icon}</div>
+              <div className='text-zinc-100'>{title}</div>
+            </a>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default Page
