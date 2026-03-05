205b3eb720ee8f62ffbee5359298ed42bd405b85
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-05 18:59:25 +0100

    Update eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@nelsonlaidev/eslint-config'
+import { defineConfig } from '@braviadev/eslint-config'
 
 export default defineConfig({
   tailwindEntryPoint: './src/styles/globals.css'
