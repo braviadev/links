fc05a3b2268bcfa0c0babcb89944a8ef6fa202af
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-05 17:57:46 +0100

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer className='mt-16 mb-8 flex justify-center'>&copy; {new Date().getFullYear()} Nelson Lai</footer>
+  return <footer className='mt-16 mb-8 flex justify-center'>&copy; {new Date().getFullYear()} Olanrewaju Toyyib</footer>
 }
 
 export default Footer
